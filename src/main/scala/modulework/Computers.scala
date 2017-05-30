fb7487bbfc2ab9ed7c6115108192499b3f6f6e30
Keith Cissell <kcissell@keith-cis-mac.local>
2017-05-30 16:58:21 -0500

    Working on restructuring the Computers.scala file

--- src/main/scala/modulework/Computers.scala
+++ src/main/scala/modulework/Computers.scala
@@ -1,38 +1,31 @@
 // src/main/scala/modulework/Computers.scala
+package modulework.Computers
 import modulework.Values._
 
 object Computers {
   abstract class Computer {
     val ram: Int
     val storage: Int
-    def getSpecs() = {
-      println(s"RAM: $ram GB")
-      println(s"HDD: $storage GB")
-    }
+    def getSpecs() = s"RAM: $ram GB" + "\n" + s"HDD: $storage GB"
   }
 
-  case class DesktopComputer(ram: Int, storage: Int, externalDevices: List[String])
-  extends Computer {
-    def getExternalDevices() = {
-      println("Devices connected to your desktop computer:")
-      externalDevices.foreach(println)
-      println
-    }
+  class DesktopComputer(ram: Int, storage: Int) extends Computer {
+    val externalDevices = List("Keyboard", "Mouse", "Printer")
+    def getExternalDevices() = "Devices connected to your desktop computer:\n" + externalDevices
   }
 
-  case class CellPhone(ram: Int, storage: Int, carrier: String)
-  extends Computer {
-    def getCarrier() = println(s"Cellphone Carrier: $carrier")
+  class CellPhone(ram: Int, storage: Int) extends Computer {
+    val carrier = "AT&T"
+    def getCarrier() = s"Cellphone Carrier: $carrier"
   }
 
   def main(args: Array[String]) = {
-    val myExternalDevices = List("Keyboard", "Mouse", "Printer")
-    val myDesktop = new DesktopComputer(16, 1000, myExternalDevices)
-    myDesktop.getSpecs()
+    val myDesktop = new DesktopComputer(16, 1000)
+    println(myDesktop.getSpecs())
     myDesktop.getExternalDevices()
 
-    val myPhone = new CellPhone(4, 64, "AT&T")
-    myPhone.getSpecs()
+    val myPhone = new CellPhone(4, 64)
+    println(myPhone.getSpecs())
     myPhone.getCarrier()
   }
 }
---
+++ src/test/scala/ComputersSpecs.scala
@@ -0,0 +1,8 @@
+// src/test/scala/ComputersSpecs.scala
+package modulework
+import org.specs2.mutable.Specification
+
+object ComputersSpecs extends Specification {
+  import modulework.Computers
+  
+}
